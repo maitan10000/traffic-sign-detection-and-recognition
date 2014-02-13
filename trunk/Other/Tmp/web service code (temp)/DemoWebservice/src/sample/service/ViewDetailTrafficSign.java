bda527c0a667ac1d8bac7253f3de035d4bfeea99
nghiahdse60628@fpt.edu.vn <nghiahdse60628@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-02-13 17:46:30 +0000

    Update WS Code

--- trunk/Other/Tmp/web service code (temp)/DemoWebservice/src/sample/service/ViewDetailTrafficSign.java
+++ trunk/Other/Tmp/web service code (temp)/DemoWebservice/src/sample/service/ViewDetailTrafficSign.java
@@ -1,19 +1,21 @@
 package sample.service;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+
 import sample.DTO.TrafficDTO;
 import sample.utility.DBConnect;
 
-public class SearchByCateID {
+public class ViewDetailTrafficSign {
 	public TrafficDTO[] searchByCateID(String id){
 		Connection con = null;
 	     try{
-	         con = DBConnect.DBConnect();         
+	    	 con = DBConnect.DBConnect();  	         
 	         String sql = "Select * from cpdb.trafficinformation where trafficID = ?";
-	         PreparedStatement stm = con.prepareStatement(sql);
+	         PreparedStatement stm = con.prepareStatement(sql);	  
 	         stm.setString(1,"%" + id + "%");
 	         ResultSet rs = stm.executeQuery();
 	         ArrayList<TrafficDTO> list = new ArrayList<TrafficDTO>();
@@ -22,10 +24,8 @@ public class SearchByCateID {
 	        	 String name = rs.getString("name");
 	        	 String image = rs. getString("image");
 	        	 String information = rs.getString("information");
-	        	 String pennaltyfee = rs.getString("pennaltyfee");
-	        	 String categoryID = rs.getString("categoryID");
-	        	 String categoryName = rs.getString("categoryName");
-	        	 TrafficDTO traffic = new TrafficDTO(trafficID,name,image,information,pennaltyfee,categoryID,categoryName);
+	        	 String pennaltyfee = rs.getString("pennaltyfee");	        	
+	        	 TrafficDTO traffic = new TrafficDTO(trafficID,name,image,information,pennaltyfee);
 	        	 list.add(traffic);
 	         }
 	         TrafficDTO[] result = new TrafficDTO[list.size()];
@@ -40,6 +40,4 @@ public class SearchByCateID {
 		
 	}
 		
-	}
-
-
+}
