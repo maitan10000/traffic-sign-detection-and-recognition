4350c1ce37cb65ebc0f82ec9127db6272abbfb85
nghiahdse60628@fpt.edu.vn <nghiahdse60628@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-02-16 15:48:50 +0000

    Update code WS

--- trunk/Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/service/LoadService.java
+++ trunk/Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/service/LoadService.java
@@ -1,35 +1,76 @@
-//package sample.service;
-//
-//import java.util.ArrayList;
-//
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//
-//import sample.dto.CategoryDTO;
-//import sample.model.*;
-//import sample.tranformer.TranformerJSON;
-//
-//@Path("/Webservice")
-//public class LoadService {
-//	@GET
-//	@Path("/GetResult")
-//	@Produces("application/json")
-//	public String messageFeed()
-//	{
-//	String result = null;
-//	try 
-//	{
-//	ArrayList<CategoryDTO> cateData = null;
-//	ProjectManager projectManager= new ProjectManager();
-//	cateData = projectManager.load();
-//	result = TranformerJSON.Cate(cateData);
-//	
-//	}
-//	catch (Exception e)
-//	{
-//	System.out.println("Exception Error"); //Console 
-//	}
-//	return result;
-//	}
-//}
+package sample.service;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import sample.dto.CategoryDTO;
+import sample.dto.TrafficInfoDTO;
+import sample.model.*;
+import sample.tranformer.TrafficInfoTrans;
+import sample.tranformer.TranformerJSON;
+
+@Path("/Webservice")
+public class LoadService {
+	@GET
+	@Path("/GetResult")
+	@Produces("application/json")
+	public String messageFeed()
+	{
+	String result = null;
+	try 
+	{
+	ArrayList<CategoryDTO> cateData = null;
+	ProjectManager projectManager= new ProjectManager();
+	cateData = projectManager.load();
+	result = TranformerJSON.Cate(cateData);
+	
+	}
+	catch (Exception e)
+	{
+	System.out.println("Exception Error"); //Console 
+	}
+	return result;
+	}
+	@GET
+	@Path("/Search")
+	@Produces("application/json")
+	public String messageFeed(@QueryParam("id") String categoryID)
+	{
+	String result = null;
+	try 
+	{
+	ArrayList<TrafficInfoDTO> trafficData = null;
+	SearchManager smn = new SearchManager();
+	trafficData = smn.search(categoryID);
+	result = TrafficInfoTrans.Traffic(trafficData);		
+	}
+	catch (Exception e)
+	{
+	e.printStackTrace();
+	}
+	return result;
+	}
+	@GET
+	@Path("/SearchbyName")
+	@Produces("application/json")
+	public String trafficName(@QueryParam("name") String name)
+	{
+	String result = null;
+	try 
+	{
+	ArrayList<TrafficInfoDTO> trafficData = null;
+	SearchManager smn = new SearchManager();
+	trafficData = smn.searchByName(name);
+	result = TrafficInfoTrans.Traffic(trafficData);		
+	}
+	catch (Exception e)
+	{
+	e.printStackTrace();
+	}
+	return result;
+	}
+}
--- trunk/Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/service/SearchByIDService.java
+++ trunk/Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/service/SearchByIDService.java
@@ -1,37 +1,37 @@
-package sample.service;
-
-import java.util.ArrayList;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import sample.dto.TrafficInfoDTO;
-import sample.model.SearchManager;
-import sample.tranformer.TrafficInfoTrans;
-
-
-@Path("/Webservice")
-public class SearchByIDService {	
-		@GET
-		@Path("/Search")
-		@Produces("application/json")
-		public String messageFeed(@QueryParam("id") String categoryID)
-		{
-		String result = null;
-		try 
-		{
-		ArrayList<TrafficInfoDTO> trafficData = null;
-		SearchManager smn = new SearchManager();
-		trafficData = smn.search(categoryID);
-		result = TrafficInfoTrans.Traffic(trafficData);		
-		}
-		catch (Exception e)
-		{
-		e.printStackTrace();
-		}
-		return result;
-		}
-		}
-
-
+//package sample.service;
+//
+//import java.util.ArrayList;
+//
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.QueryParam;
+//import sample.dto.TrafficInfoDTO;
+//import sample.model.SearchManager;
+//import sample.tranformer.TrafficInfoTrans;
+//
+//
+//@Path("/Webservice")
+//public class SearchByIDService {	
+//		@GET
+//		@Path("/Search")
+//		@Produces("application/json")
+//		public String messageFeed(@QueryParam("id") String categoryID)
+//		{
+//		String result = null;
+//		try 
+//		{
+//		ArrayList<TrafficInfoDTO> trafficData = null;
+//		SearchManager smn = new SearchManager();
+//		trafficData = smn.search(categoryID);
+//		result = TrafficInfoTrans.Traffic(trafficData);		
+//		}
+//		catch (Exception e)
+//		{
+//		e.printStackTrace();
+//		}
+//		return result;
+//		}
+//		}
+//
+//
