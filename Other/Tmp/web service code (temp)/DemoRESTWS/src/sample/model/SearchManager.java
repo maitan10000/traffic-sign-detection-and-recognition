627f786ac7a4473849e165e68be0f473fd3659b7
nghiahdse60628@fpt.edu.vn <nghiahdse60628@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-02-17 15:50:21 +0000

    Update code

--- Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/model/SearchManager.java
+++ Other/Tmp/web service code (temp)/DemoRESTWS/src/sample/model/SearchManager.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 
 
 
+
 import sample.dto.TrafficInfoDTO;
 import sample.utility.DBConnect;
 import sample.utility.SearchByCateID;
 import sample.utility.SearchByName;
+import sample.utility.ViewDetailTraffic;
 
 public class SearchManager {
 	public ArrayList<TrafficInfoDTO> search(String categoryID)throws Exception {
@@ -32,6 +34,19 @@ public class SearchManager {
 			    Connection connection = database.Get_Connection();
 			    SearchByName tmp = new SearchByName();
 				traffic= tmp.searchByName(connection, name);
+		
+		} catch (Exception e) {
+			throw e;
+		}
+		return traffic;
+	}
+	public ArrayList<TrafficInfoDTO> viewDetail(String trafficID)throws Exception {
+		ArrayList<TrafficInfoDTO> traffic = null;
+		try {
+			    DBConnect database= new DBConnect();
+			    Connection connection = database.Get_Connection();
+			    ViewDetailTraffic tmp = new ViewDetailTraffic();
+				traffic= tmp.viewDetail(connection, trafficID);
 		
 		} catch (Exception e) {
 			throw e;
