197637878751f886a1c0815c234c9d109b7c054a
nghiahdse60628@fpt.edu.vn <nghiahdse60628@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-03-15 16:03:41 +0000

    Update Code WS

---
+++ Other/Tmp/web service code (temp)/Version 1.1/Traffic/src/main/java/dto/FavoriteJSON.java
@@ -0,0 +1,45 @@
+package dto;
+
+public class FavoriteJSON {
+	private String creator;
+	private String trafficID;
+	private String name;
+	private String image;
+	private Boolean isActive;
+	public FavoriteJSON() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getTrafficID() {
+		return trafficID;
+	}
+	public void setTrafficID(String trafficID) {
+		this.trafficID = trafficID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+
+}
