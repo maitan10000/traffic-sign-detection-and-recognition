156e17b49a9f6631827fbf1ae3cc6c6a23da8c6f
tanmvse90061@fpt.edu.vn <tanmvse90061@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-03-19 06:29:01 +0000

    Add Search Auto function

---
+++ trunk/Other/Tmp/website template/Version 1.0/TrafficWeb/src/main/java/listener/ContextListener.java
@@ -0,0 +1,39 @@
+package listener;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("application context listener")
+public class ContextListener implements ServletContextListener {
+
+	public void contextInitialized(ServletContextEvent event) {
+		// Init global values
+		Properties prop = new Properties();
+		FileInputStream in;
+		try {
+			String realPath = event.getServletContext().getRealPath(
+					"/WEB-INF/config.properties");
+			in = new FileInputStream(realPath);
+			prop.load(in);
+			//GlobalValue.WorkPath = prop.getProperty("workPath").trim();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		// do nothing
+	}
+}
