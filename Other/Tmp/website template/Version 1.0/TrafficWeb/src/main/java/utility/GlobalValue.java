9e8c4f75a36349499a13e29457d0f4fb4690ea7f
tanmvse90061@fpt.edu.vn <tanmvse90061@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-03-23 03:50:22 +0000

    Change GlobalValue class

--- Other/Tmp/website template/Version 1.0/TrafficWeb/src/main/java/utility/GlobalValue.java
+++ Other/Tmp/website template/Version 1.0/TrafficWeb/src/main/java/utility/GlobalValue.java
@@ -1,5 +1,40 @@
 package utility;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
 public class GlobalValue {
-	public static String ServiceAddress = "";
+	private static String serviceAddress = "";
+	private static boolean isCreated = false;
+
+	public static void createInstance(String configPath) throws Exception {
+		if (isCreated == false) {
+			isCreated = true;
+			Properties prop = new Properties();
+			FileInputStream in;
+			try {
+				in = new FileInputStream(configPath);
+				prop.load(in);
+				
+				//map value
+				serviceAddress = prop.getProperty("ServiceAddress").trim();
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			throw new Exception("Cannot recreate new instance");
+		}
+	}
+
+	public static String getServiceAddress() {
+		return serviceAddress;
+	}
 }
