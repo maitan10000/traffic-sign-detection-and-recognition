313b703db03ed3d8dfe849f50043f73b7c0f46aa
tanmvse90061@fpt.edu.vn <tanmvse90061@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-04-23 03:35:24 +0000

    Add service to download mobile app

---
+++ Source Code/Code/Service/Version 2.0/TrafficService/src/main/java/service/Download.java
@@ -0,0 +1,31 @@
+package service;
+
+import java.io.File;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+
+import utility.GlobalValue;
+
+@Path("/Download")
+public class Download {
+	@GET
+	@Path("/mobile/android")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response getAndroidApp() {
+		String androidLastVersionPath = GlobalValue.getWorkPath()+"Mobile/TrafficSignRecognition.apk";
+		File file = new File(androidLastVersionPath);
+		if (file.exists()) {
+			ResponseBuilder response = Response.ok((Object) file);
+			response.header("Content-Disposition",
+					"attachment; filename=\"Nhan dang bien bao giao thong.apk\"");
+			return response.build();
+		}
+		return Response.status(Status.NOT_FOUND).build();
+	}
+}
