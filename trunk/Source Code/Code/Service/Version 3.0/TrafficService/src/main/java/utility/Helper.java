f15d6442e38dee13f30fa46713abca3fd674139f
tanmvse90061@fpt.edu.vn <tanmvse90061@fpt.edu.vn@b643f4b9-1c65-7add-5b1f-633df3465b5b>
2014-05-11 04:01:21 +0000

    Add show image train num

--- trunk/Source Code/Code/Service/Version 3.0/TrafficService/src/main/java/utility/Helper.java
+++ trunk/Source Code/Code/Service/Version 3.0/TrafficService/src/main/java/utility/Helper.java
@@ -16,7 +16,7 @@ import org.openxmlformats.schemas.drawingml.x2006.spreadsheetDrawing.STEditAs;
 import json.ResultInput;
 
 public class Helper {
-
+	private static int tradeOffValue = 100;
 	/**
 	 * Run TSRT with list command
 	 * 
@@ -27,7 +27,42 @@ public class Helper {
 	public static String runTSRT(String appDic, ArrayList<String> listCommand) {
 		String commands = "./TSRT";
 		listCommand.add(0, commands);
+		String result = executeCommand(appDic, listCommand);
+		if (result.contains("Killed")) {
+			result = trainSVMOnly(appDic, tradeOffValue);
+			if (result.contains("done")) {
+				result = "";
+			}
+		}
+		return result;
+	}
 
+	/**
+	 * Train DB using SVM
+	 * 
+	 * @param appDic
+	 * @return
+	 */
+	public static String trainSVM(String appDic) {
+		ArrayList<String> listCommand = new ArrayList<String>();
+		listCommand.add("-l");
+		listCommand.add(tradeOffValue+"");
+		return runTSRT(appDic, listCommand);
+	}
+
+	public static String trainSVMOnly(String appDic, int tradeOffValue) {
+		appDic += "Data/SVM_Multiclass";
+		ArrayList<String> listCommand = new ArrayList<String>();
+		listCommand.add("./svm_multiclass_learn");
+		listCommand.add("-c");
+		listCommand.add(tradeOffValue + "");
+		listCommand.add("saveFeatures.txt");
+		listCommand.add("model.txt");
+		return executeCommand(appDic, listCommand);
+	}
+
+	private static String executeCommand(String appDic,
+			ArrayList<String> listCommand) {
 		// Run macro on target
 		ProcessBuilder pb = new ProcessBuilder();
 		pb.command(listCommand);
@@ -62,19 +97,6 @@ public class Helper {
 	}
 
 	/**
-	 * Train DB using SVM
-	 * 
-	 * @param appDic
-	 * @return
-	 */
-	public static String trainSVM(String appDic) {
-		ArrayList<String> listCommand = new ArrayList<String>();
-		listCommand.add("-l");
-		listCommand.add("10000");
-		return runTSRT(appDic, listCommand);
-	}
-
-	/**
 	 * Write InputStream to File
 	 * 
 	 * @param inputStream
@@ -100,6 +122,7 @@ public class Helper {
 
 	/**
 	 * Write byte[] to file
+	 * 
 	 * @param data
 	 * @param fileLocation
 	 */
